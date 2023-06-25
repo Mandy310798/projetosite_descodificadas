02bbde86ae581fa79e75c235a6c151f8dbef7c4f
Mandy310798 <amanda_kenshi@hotmail.com>
2023-06-25 18:39:42 -0300

    responsividade e javascript

---
+++ script.js
@@ -0,0 +1,49 @@
+function slide1(){
+    document.getElementById('slider-image').src="Site_Imagens/Motivacional.png";
+    setTimeout("slide2()", 3000)
+}
+
+function slide2(){
+    document.getElementById('slider-image').src="Site_Imagens/Capa personalizadas.png";
+    setTimeout("slide3()", 3000)
+}
+
+function slide3(){
+    document.getElementById('slider-image').src="Site_Imagens/Capa religião.png";
+    setTimeout("slide4()", 3000)
+}
+
+function slide4(){
+    document.getElementById('slider-image').src="Site_Imagens/CapaAmizade.png";
+    setTimeout("slide5()", 3000)
+}
+
+function slide5(){
+    document.getElementById('slider-image').src="Site_Imagens/CapaAmor.png";
+    setTimeout("slide6()", 3000)
+}
+
+function slide6(){
+    document.getElementById('slider-image').src="Site_Imagens/CapaBichinhos.png";
+    setTimeout("slide7()", 3000)
+}
+
+function slide7(){
+    document.getElementById('slider-image').src="Site_Imagens/CapaFocinhos.png";
+    setTimeout("slide8()", 3000)
+}
+
+function slide8(){
+    document.getElementById('slider-image').src="Site_Imagens/Capa F1.png";
+    setTimeout("slide9())", 3000)
+}
+
+function slide9(){
+    document.getElementById('slider-image').src="Site_Imagens/CapaDivertidas.png";
+    setTimeout("slide10()", 3000)
+}
+
+function slide10(){
+    document.getElementById('slider-image').src="Site_Imagens/Personalizadas.png";
+    setTimeout("slide1()", 3000)
+}
